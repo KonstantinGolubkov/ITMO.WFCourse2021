b768e30b28ab09f27fb437c2d01c7189ff8c14f6
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-06-18 21:47:22 +0300

    Create new project

--- ITMO.WFCourse2021.Lab01_01.Rectangle/Form1.Designer.cs
+++ ITMO.WFCourse2021.Lab01_01.Rectangle/Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(97, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Border Style";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(294, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 69);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Resize";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(499, 264);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 69);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Opacity";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Location = new System.Drawing.Point(100, 200);
+            this.Name = "Form1";
+            this.Opacity = 0.75D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Trey Research";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
