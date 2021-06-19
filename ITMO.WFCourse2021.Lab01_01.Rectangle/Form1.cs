23bb24fd49b36f143e7c2d1015f64b173fdee39a
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-06-19 11:09:50 +0300

    Add ObjectDisposedException

--- ITMO.WFCourse2021.Lab01_01.Rectangle/Form1.cs
+++ ITMO.WFCourse2021.Lab01_01.Rectangle/Form1.cs
@@ -12,9 +12,12 @@ namespace ITMO.WFCourse2021.Lab01_01.Rectangle
 {
     public partial class Form1 : Form
     {
+        nForm myF2;
+
         public Form1()
         {
             InitializeComponent();
+            myF2 = new nForm();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -34,10 +37,23 @@ namespace ITMO.WFCourse2021.Lab01_01.Rectangle
 
         private void button4_Click(object sender, EventArgs e)
         {
-            nForm myF2 = new nForm();
             myF2.StartPosition = FormStartPosition.Manual;
             myF2.Location = new Point(this.Location.X + this.Width, this.Location.Y);
-            myF2.Show();
+
+            try
+            {
+                myF2.Show();
+                myF2.Activate();
+            }
+            catch (ObjectDisposedException ex)
+            {
+                myF2 = new nForm();
+                myF2.Text = "Повторное создание формы";
+                myF2.StartPosition = FormStartPosition.Manual;
+                myF2.Location = new Point(this.Location.X + this.Width, this.Location.Y);
+                myF2.Show();
+                myF2.Activate();
+            }
         }
     }
 }
