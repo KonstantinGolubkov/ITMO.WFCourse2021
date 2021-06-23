72223eb175448fabb7129a950b1fc1b05353aaf0
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-06-23 20:56:01 +0300

    Add project Lab01_05.ControlTask2.Rhomb

---
+++ ITMO.WFCourse2021.Lab01_05.ControlTask2.Rhomb/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.WFCourse2021.Lab01_05.ControlTask2.Rhomb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath myPath = new System.Drawing.Drawing2D.GraphicsPath();
+            myPath.AddLines(new[]
+                                    {
+                                        new Point(0, Height / 2),
+                                        new Point(Width / 2, 0),
+                                        new Point(Width, Height / 2),
+                                        new Point(Width / 2, Height)
+                                    }
+                            );
+            //myPath.AddEllipse(0, 0, this.Width, this.Height);
+            //myPath.AddPolygon(new Point[] { new Point(0, 0), new Point(0, this.Height), new Point(this.Width, 0) });
+            Region myRegion = new Region(myPath);
+            this.Region = myRegion;
+
+        }
+    }
+}
---
+++ ITMO.WFCourse2021.Lab01_05.ControlTask2.Rhomb/Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.WFCourse2021.Lab01_05.ControlTask2.Rhomb
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 myF1 = new Form1();
+            myF1.Show();
+        }
+    }
+}
